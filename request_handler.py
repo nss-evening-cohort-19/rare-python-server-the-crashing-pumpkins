a712079dd9b3db6bb685894695ce85d88f38f950
Michael Pennington <mwpennington@live.com>
2022-11-02 10:12:28 -0500

    pulling the newest main

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-from views.user import create_user, login_user
+from views.user_request import create_user, login_user
 
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -51,8 +51,14 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     def do_GET(self):
         """Handle Get requests to the server"""
-        pass
-
+        self._set_headers(200)
+        response = ''
+        resource, key = self.parse_url()
+        
+        if resource == 'user':
+            response = 'The key is that I know'
+            
+        self.wfile.write(response.encode())
 
     def do_POST(self):
         """Make a post request to the server"""
